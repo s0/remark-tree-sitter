1d2d6e0ce3d3b90a0d980b8664db1cbde6e713b3
Sam Lanning <sam@samlanning.com>
2019-06-13 17:20:39 -0700

    Add typescript boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var gutil = require("gulp-util");
+var ts = require('gulp-typescript');
+var tslint = require('tslint');
+var gulpTslint = require('gulp-tslint');
+var runSequence = require('run-sequence');
+
+var tsProject = ts.createProject('src/tsconfig.json');
+
+// Utility Functions
+
+function handleError(err) {
+  gutil.log("Build failed", err.message);
+  process.exit(1);
+}
+
+gulp.task('clean', function() {
+  return gulp.src(['dist'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('ts', function () {
+    return tsProject.src()
+      .pipe(tsProject())
+      .on('error', handleError)
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy-package-json', function () {
+    return gulp.src(['package.json']).pipe(gulp.dest('dist'));
+});
+
+gulp.task('tslint', function() {
+  var program = tslint.Linter.createProgram("src/tsconfig.json");
+
+  return gulp.src(['src/**/*.ts'])
+  .pipe(gulpTslint({
+    formatter: 'verbose',
+    configuration: 'tslint.json',
+    program
+  }))
+  .on('error', handleError)
+  .pipe(gulpTslint.report());
+});
+
+gulp.task('default', function(callback) {
+  runSequence(
+    'clean',
+    ['ts', 'copy-package-json'],
+    'tslint',
+    callback);
+});
